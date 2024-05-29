9550acc57ea679c44be4b681131c563a3e9bb0d9
Daniel Jarski <30324210+QuantumToasted@users.noreply.github.com>
2024-05-29 13:31:05 -0500

    Add simple error response model

---
+++ DTOs/ErrorResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Text.Json.Serialization;
+
+namespace OpenFastDL.Api;
+
+public sealed record ErrorResponseDTO(
+    [property: JsonPropertyName("code")] HttpStatusCode StatusCode,
+    [property: JsonPropertyName("message")] string Message)
+{
+    public static ErrorResponseDTO Conflict(string message)
+        => new(HttpStatusCode.Conflict, message);
+
+    public static ErrorResponseDTO BadRequest(string message)
+        => new(HttpStatusCode.BadRequest, message);
+}
